833776be8ff88d22243fac3b8c11d04973c39dba
Timothy Lee <timothylkx@hotmail.com>
2024-05-31 16:53:13 +0800

    segwayrmp_bringup: combined segwayrmp_navigation_launch.py and segwayrmp_localization_launch.py into segwayrmp_bringuplaunch.py; nav2_param.yaml: tuned inflation layer, noise, turning radius and speed

--- src/segwayrmp_bringup/launch/segwayrmp_localization_launch.py
+++ src/segwayrmp_bringup/launch/segwayrmp_localization_launch.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2018 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -27,12 +13,8 @@ from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
-    segwayrmp_bringup_dir = os.path.join(
-        os.path.expanduser("~"),
-        "segwayrmp_ros2",
-        "src",
-        "segwayrmp_bringup",
-    )
+    # Get the launch directory    
+    segwayrmp_bringup_dir = get_package_share_directory("segwayrmp_bringup")
 
     namespace = LaunchConfiguration("namespace")
     map_yaml_file = LaunchConfiguration("map")
@@ -78,7 +60,6 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         "map",
-        default_value=os.path.join(segwayrmp_bringup_dir, "maps", "office.yaml"),
         description="Full path to map yaml file to load",
     )
 
