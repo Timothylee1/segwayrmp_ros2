5bc2cc550f527b73d649ddf8d31c6f88ed302bad
timothy <timothylkx@hotmail.com>
2024-07-01 09:14:19 +0800

    Included costmap_converter for teb controler

--- src/segwayrmp_description/launch/gazebo.launch.py
+++ src/segwayrmp_description/launch/gazebo.launch.py
@@ -1,20 +1,68 @@
-<launch>
-  <include
-    file="$(find gazebo_ros)/launch/empty_world.launch" />
-  <node
-    name="tf_footprint_base"
-    pkg="tf"
-    type="static_transform_publisher"
-    args="0 0 0 0 0 0 base_link base_footprint 40" />
-  <node
-    name="spawn_model"
-    pkg="gazebo_ros"
-    type="spawn_model"
-    args="-file $(find 401pius)/urdf/401pius.urdf -urdf -model 401pius"
-    output="screen" />
-  <node
-    name="fake_joint_calibration"
-    pkg="rostopic"
-    type="rostopic"
-    args="pub /calibrated std_msgs/Bool true" />
-</launch>
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+    # Launch configuration variables
+    use_sim_time = LaunchConfiguration("use_sim_time", default="True")
+    launch_rviz = LaunchConfiguration("launch_rviz", default="False")
+
+    # Include SLAM toolbox launch file from slam_toolbox package
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                FindPackageShare("slam_toolbox").find("slam_toolbox"),
+                "launch",
+                "online_async_launch.py"
+            )
+        ),
+        launch_arguments={"use_sim_time": use_sim_time}.items(),
+    )
+
+    # Get the source directory of the package
+    segwayrmp_description_src = os.path.join(
+        FindPackageShare("segwayrmp_description").find("segwayrmp_description"),
+        os.pardir,
+        os.pardir,
+        "src",
+        "segwayrmp_description"
+    )
+
+    # Define the path to the RViz configuration file
+    rviz_config_file_full_path = os.path.join(segwayrmp_description_src, "rviz", "default_view.rviz")
+
+    # Launch RViz with the specific configuration file
+    rviz2_launch = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "run",
+            "rviz2",
+            "rviz2",
+            "-d",
+            rviz_config_file_full_path,
+        ],
+        output="screen",
+        condition=IfCondition(launch_rviz),
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="True",
+                description="Use simulation (Gazebo) clock if true",
+            ),
+            DeclareLaunchArgument(
+                "launch_rviz",
+                default_value="False",
+                description="Launch RViz if set to true",
+            ),
+            slam_toolbox_launch,
+            rviz2_launch,
+        ]
+    )
